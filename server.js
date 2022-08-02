ed99ab942a589ca93f8ca88e45f613f95d622636
Abhay Singh Rajput <abhayrajput843@gmail.com>
2022-08-02 21:57:55 +0530

    deployed

--- server.js
+++ server.js
@@ -140,6 +140,6 @@ app.get('/404', (req, res) => {
 app.use((req, res) => {
     res.redirect('/404');
 })
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000');
 })
